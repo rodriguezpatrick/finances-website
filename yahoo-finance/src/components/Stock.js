de6e298c3fbaf8232d4c1f20f576a57a5c803e44
Patrick Rodrigues <patrickrodrigues740@gmail.com>
2024-05-21 17:13:19 -0300

    Backend Real-time stock ticker feito

--- yahoo-finance/src/components/Stock.js
+++ yahoo-finance/src/components/Stock.js
@@ -1,12 +1,42 @@
-import styles from "./Stock.module.css"
+import styles from "./Stock.module.css";
+import { useEffect, useState } from "react";
+import protobuf from "protobufjs";
+const { Buffer } = require("buffer/");
 
-function Stock(){
+function Stock() {
+        const[stock, setStock] = useState(null);
 
-        return(
-<div className={styles.Stock}>
-    
-</div>
-        )
+  useEffect(() => {
+    const ws = new WebSocket("wss://streamer.finance.yahoo.com");
+    protobuf.load("./YPricingData.proto", (error, root) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      const Yaticker = root.lookupType("yaticker");
+
+      ws.onopen = function open() {
+        console.log("connected");
+        ws.send(
+          JSON.stringify({
+            subscribe: ["PETR4.SA"],
+          })
+        );
+      };
+
+      ws.onclose = function close() {
+        console.log("disconnected");
+      };
+
+      ws.onmessage = function incoming(message) {
+        const next = Yaticker.decode(new Buffer(message.data, "base64"));
+        setStock(next)
+      };
+    });
+  }, []);
+  return <div className={styles.Stock}>
+        {stock && <h2>{stock.id}: {stock.price}</h2>}
+  </div>;
 }
 
-export default Stock
+export default Stock;
