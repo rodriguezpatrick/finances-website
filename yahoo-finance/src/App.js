36c72f3851a0aed3f110ed1756ceb57fae09658e
Patrick Rodrigues <patrickrodrigues740@gmail.com>
2024-06-04 08:37:02 -0300

    Retirando imports de pastas apagadas do app.js

--- yahoo-finance/src/App.js
+++ yahoo-finance/src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import Stock from "./components/organisms/stock/Stock";
-import InputStock from "./components/organisms/inputStock/inputStock";
 import InputButton from "./components/molecules/inputButton/InputButton";
 
 function App() {
